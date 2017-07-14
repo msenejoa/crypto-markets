31f303a4148ec4f80d92637aae74af3b5fc82fcc
Michael <michaelsenejoa@gmail.com>
2017-07-13 22:48:11 -0400

    removed commented out code

--- components/graphs/dateConverter.js
+++ components/graphs/dateConverter.js
@@ -1,25 +1,23 @@
 const Converter = (data) => {
   var obj = [];
   data.forEach((a, i) => {
+    /*
     var day = new Date(a.time *1000);
-    //console.log(day);
     var hours = day.getHours();
     var minutes = "0" + day.getMinutes();
     var seconds = "0" + day.getSeconds();
     var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    //console.log(formattedTime);
+    */
+    
     obj.push({
       x : i,
       y : a.close
     });
-    //var newObject = Object.assign(data, indexNumber);
-    //console.log(newObject[i]);
-    //console.log(a.time);
-    //console.log(a.close);
+
   });
-  //console.log(data[0].time * 1000);
+
   console.log('----------------------------------------------------------');
-  //console.log(obj);
+
   var obj2 = [];
   obj2.push(obj);
   return obj2;
